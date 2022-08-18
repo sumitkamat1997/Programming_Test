f42480c0826272d00f5b91c547e11cf21ee6ccd7
SUMIT KUMAR KAMTI <82934793+sumitkamat1997@users.noreply.github.com>
2022-08-18 18:23:07 +0530

    Create Program-2.java

---
+++ Program-2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Singint{
+    public static void main(String[] args){
+       
+        Input ob=new Input();
+        ob.series();
+    }
+}
+class Input{
+    
+      ArrayList<Integer> list=new ArrayList<Integer>();
+      void series(){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter input a-");
+      int n=sc.nextInt();
+      int c=1;
+      for(int i=0;i<n;i++){
+         
+        list.add(c);
+        c=c+2;
+      }
+      System.out.print("then output : "+list);
+    }
+}
